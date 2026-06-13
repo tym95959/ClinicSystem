742e05c9e9fcc591350a47df5b72d1d8f54c64bc
tym95959 <tym95959@gmail.com>
2026-06-13 20:35:38 +0500

    Create device-config.js

---
+++ device-config.js
@@ -0,0 +1,6 @@
+// DEVICE CONFIGURATION FILE
+// Store this on your server - users can still view it if they know the path
+window.DEVICE_CONFIG = {
+    deviceMasterPassword: "HopeDental2025!",
+    authExpiryDays: 365
+};
